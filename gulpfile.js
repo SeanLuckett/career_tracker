90848e4245cb4f2818f1d05b395a6a3cc6099348
Sean Luckett <sluckett@itriagehealth.com>
2015-09-14 18:20:16 -0600

    Adds gulp live server. Changes src/ to app/

--- gulpfile.js
+++ gulpfile.js
@@ -5,21 +5,35 @@ var gulp = require('gulp'),
     sass = require('gulp-ruby-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     notify = require('gulp-notify'),
+    gls = require('gulp-live-server'),
     concat = require('gulp-concat');
 
 gulp.task('default', function() {
   console.log('You done launched the default task.');
 });
 
+gulp.task('serve', ['babel-dev', 'styles', 'html', 'watch'], function() {
+  var server = gls.static(['dev_build']);
+  server.start();
+
+  gulp.watch([
+    './dev_build/**/*.js',
+    './dev_build/**/*.html',
+    '.dev_build/**/*.css'
+  ], function(file) {
+    server.notify.apply(server, [file]);
+  });
+});
+
 gulp.task('lint', function() {
-  return gulp.src(['./src/**/*.es6'])
+  return gulp.src(['./app/**/*.es6'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failOnError());
 });
 
 gulp.task('babel-dev', ['lint'], function() {
-  return gulp.src(['./src/**/*.module.es6', './src/**/*.es6'])
+  return gulp.src(['./app/**/*.module.es6', './app/**/*.es6'])
     .pipe(sourcemaps.init())
     .pipe(babel())
     .pipe(concat('app.js'))
@@ -28,14 +42,21 @@ gulp.task('babel-dev', ['lint'], function() {
 });
 
 gulp.task('styles', function() {
-  return sass('./src/**/*.scss', {style: 'expanded'})
+  return sass('./app/**/*.scss', {style: 'expanded'})
     .pipe(autoprefixer('last 2 version'))
     .pipe(gulp.dest('./dev_build/css/'))
     .pipe(concat('main.css'))
     .pipe(notify({message: 'Styles task complete'}));
 });
 
+gulp.task('html', function() {
+  return gulp.src(['./app/**/*.html'])
+    .pipe(gulp.dest('./dev_build'));
+});
+
 gulp.task('watch', ['babel-dev'], function() {
-  gulp.watch('./src/**/*.es6', ['babel-dev']);
+  gulp.watch('./app/**/*.es6', ['babel-dev']);
+  gulp.watch('./app/**/*.scss', ['styles']);
+  gulp.watch('./app/**/*.html', ['html']);
 });
 
